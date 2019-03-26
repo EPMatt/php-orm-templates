06b35fe60e43768815ae00863b44439b1d21a5d6
EPMatt <acc.epmatt@gmail.com>
2019-03-26 22:47:50 +0100

    do class abstraction

--- sources/do.php
+++ sources/do.php
@@ -4,49 +4,34 @@
  * Date:        11/02/2019
  * ORM Templates - Data Object Class
  */
-class DataObject {
-
-    /**
-     * Construct an instance with the given values
-     */
-    public function __construct() {
-    }
-
+abstract class DataObject {
  
     /**
      * Get an associative array representing the object
      * @return array the associative array corresponding to the current DataObject instance
      */
-    public function getArray(){
-        return array();
-    }
+    abstract public function getArray();
     
 
     /**
      * Get an associative array containing the primary key of the object
      * @return array the associative array corresponding to the current DataObject instance primary key
      */
-    public function getPrimaryKey(){
-        return array();
-    }
+    abstract public function getPrimaryKey();
 
 
     /**
      * Get an associative array containing the fields of the object (primary key excluded)
      * @return array the associative array corresponding to the current DataObject instance fields, primary key excluded
      */
-    public function getFields(){
-        return array();
-    }
+    abstract public function getFields();
 
      /**
      * Construct an instance from a record
      * @param array $fields associative array corresponding to a record in the table
      * @return DataObject $do the Data Object instance corresponding to the given array
      */
-    public static function fromArray(array $fields) {
-        return new DataObject();
-    }
+    abstract public static function fromArray(array $fields);
 
 
 }
